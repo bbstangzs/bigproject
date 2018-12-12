8fd45783333f169b10128f68a9cb6ef70fef8e8c
bbstangzs <43695627+bbstangzs@users.noreply.github.com>
2018-12-12 06:44:48 -0600

    Add files via upload

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-#!/usr/local/bin/python3
 import pickle,random,getpass,os,time,sys
 from datetime import datetime
 
@@ -15,12 +14,13 @@ def new_game ():
         if passwd == passwd2:
             break
         print("两次密码不一致")
-    player_info={"username":username,"passwd":passwd,"liliang":10,"minjie":10,"tizhi":10,"zhili":10}
+    player_info={"username":username,"passwd":passwd,"money":100,"level":1,"exp":0,"liliang":10,"minjie":10,"tizhi":10,"zhili":10}
     with open(data,"wb") as file:
         pickle.dump(player_info,file)
     return player_info
 
-def save_game (username,play_info):
+def save_game (play_info):
+    username=play_info["username"]
     data=os.path.join("/tmp/save",username+".txt")
     with open(data,"wb") as file:
         pickle.dump(play_info,file)
@@ -40,7 +40,7 @@ def load_game ():
                 print("用户名不存在或密码错误!")
     return player_info
 
-def exit_game ():
+def exit_game (n):
     mksure="""
 请确认是否已存档
 1)已存档,退出
@@ -71,7 +71,7 @@ def main_menu ():
         break
     return ch_list[choice]()
 
-def main_game (player_info):
+def init_game ():
     print("进入游戏")
     x=0
     while x < 3:
@@ -83,9 +83,42 @@ def main_game (player_info):
             y+=1
         x+=1
     print("\n")
-    print(player_info)
 
+def hit_enemy (n):
+    print("待开发")
+
+def shuxing (n):
+    print("人物名:%s\t金钱:%d"% (n["username"],n["money"]))
+    print("等级:%d\t\t经验:%d"% (n["level"],n["exp"]))
+    print("力量:%d\t\t敏捷:%d"% (n["liliang"],n["minjie"]))
+    print("体质:%d\t\t智力:%d"% (n["tizhi"],n["zhili"]))
+
+def package (n):
+    print("背包是空的,快去收集点资源吧!")
+
+def randitem (n):
+    print("遇到魔王,没打过,你死了!")
+    exit()
+
+def main_game (n):
+    tips='''---主菜单---
+1)打怪升级  
+2)查看属性
+3)查看背包
+4)随机事件
+5)存档
+6)退出
+:'''
+    ch_list={"1":hit_enemy,"2":shuxing,"3":package,"4":randitem,"5":save_game,"6":exit_game}
+    while True:
+        choice = input(tips)
+        if choice not in ["1","2","3","4","5","6"]:
+            choice=input("输入错误!请重新输入(1-6):")
+        ch_list[choice](n)
+        break
 
 if __name__ == "__main__":
     player_info=main_menu()
-    main_game(player_info)
+    init_game()
+    while True:
+        main_game(player_info)
